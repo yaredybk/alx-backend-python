24b792f67f4fafcd2eb50e625c4eb10b2786ee5e
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-08-27 01:31:49 +0300

    fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,9 +13,9 @@ from utils import (access_nested_map, get_json, memoize)
 class TestAccessNestedMap(unittest.TestCase):
     """Test Access nested map with key path."""
     @parameterized.expand([
-        ("simple_map",{"a": 1}, ("a"),(1)),
-        ("nested_map",{"a": {"b": 2}}, ("a"), ({"b": 2})),
-        ("nested_map_access",{"a": {"b": 2}}, ("a", "b"), (2))
+        ("simple_map", {"a": 1}, ("a"),(1)),
+        ("nested_map", {"a": {"b": 2}}, ("a"), ({"b": 2})),
+        ("nested_map_access", {"a": {"b": 2}}, ("a", "b"), (2))
     ])
     def test_access_nested_map(self, name: str, nested_map: Dict[str, Any],
                                path: Tuple[str, ...], expected: Tuple[Any, ...]
@@ -24,8 +24,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
     
     @parameterized.expand([
-        ("simple_map",{}, ("a")),
-        ("nested_map",{"a": 1}, ("a", "b"))
+        ("simple_map", {}, ("a")),
+        ("nested_map", {"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, name: str,
                                          nested_map: Dict[str, Any],
